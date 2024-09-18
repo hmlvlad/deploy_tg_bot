cbaaabee8ec41902d72566061725f0c00098b884
hmlvlad <4HnGJkUYaBk77m@>
2024-09-19 02:22:11 +0300

    112222221

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello Dimon test!")
+    await message.answer("Hello Dimon 22222222222222222222!")
 
 @dp.message()
 async def send_doc(message):
